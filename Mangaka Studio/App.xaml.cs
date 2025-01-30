6d3d279ae308f6a3df71ea23d0519ef3054ef1d9
FanaticCarp32 <vlad-shupletsov@mail.ru>
2025-01-30 20:03:23 +0300

    Создание пустого проекта.

---
+++ Mangaka Studio/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Mangaka_Studio
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Mangaka Studio/Controls/IconButton.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Mangaka_Studio.Controls
+{
+    public class IconButton : Button
+    {
+        public static readonly DependencyProperty dependencyProperty =
+            DependencyProperty.Register(
+                "IconSource",
+                typeof(string),
+                typeof(IconButton),
+                new PropertyMetadata(null)
+            );
+
+        public string IconSource
+        {
+            get { return (string)GetValue(dependencyProperty); }
+            set { SetValue(dependencyProperty, value); }
+        }
+    }
+}
